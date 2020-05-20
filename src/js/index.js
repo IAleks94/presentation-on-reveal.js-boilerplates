57a00dde1b1c993f288783e85bd505d249c394ee
IAleks94 <a12344@mail.ru>
2020-05-20 16:26:08 +0300

    feat: add slides

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,27 @@
 import Reveal from 'reveal.js';
+
+// import hljs from 'highlight.js/lib/core';
+// import javascript from 'highlight.js/lib/languages/javascript';
+// import xml from 'highlight.js/lib/languages/xml';
+// hljs.registerLanguage('xml', xml);
 import 'sass/main.scss';
 
 document.body.innerHTML = `<div class="reveal">
 <div class="slides">
     <!--inject:slides-->
-    <section>
-      <img data-src="images/logo.webp">
-    </section>
-    <section>Slide 2</section>
+    <section><h1>The END</h1></section>
 </div>
 </div>`;
 
 Reveal.initialize({
-  controls: false,
+  controls: true,
   touch: true,
+  slideNumber: 'c/t',
+  minScale: 0.2,
+  maxScale: 2.0,
   dependencies: [
     { src: 'plugin/highlight/highlight.js', async: true },
+    { src: 'plugin/highlight/markdown.js', async: true },
     { src: 'plugin/notes/notes.js', async: true },
   ],
 });
